5607fd9cf958311505699a92095cbe9107b643cf
jihyuky <jihyuky@gmail.com>
2023-09-11 16:12:07 -0400

    multiple main method

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int counter = 0;
+        while(i<1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                counter += 1;
+            }
+            i++;
+        }
+        System.out.println(counter);
+
+    }
+}

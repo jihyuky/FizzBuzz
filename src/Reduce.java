b92dc92ca7acf603fb195c04901d1cf72365fe17
jihyuky <jihyuky@gmail.com>
2023-09-11 16:14:34 -0400

    reduce method added

---
+++ src/Reduce.java
@@ -0,0 +1,11 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int counter = 0;
+        while(i > 0){
+            i--;
+            counter ++;
+        }
+        System.out.println(counter);
+    }
+}
